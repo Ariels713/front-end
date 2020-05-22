48d05a3f809ed7cabded1a3f94292eba827920b8
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-05-21 23:15:05 -0400

    added image to campange page

--- sixr/src/components/CreateCampaign.js
+++ sixr/src/components/CreateCampaign.js
@@ -9,8 +9,21 @@ import {
   Button,
   MenuItem,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import headerImage from "../assets/campaint_main_image.svg";
 //import the action
 //make a reducer for the create campaign page
+
+const useStyles = makeStyles((theme) => ({
+  mainImage: {
+    backgroundImage: `url(${headerImage})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100%",
+    width: "100%",
+  },
+}));
 
 const projectTypes = [
   {
@@ -40,89 +53,102 @@ const projectTypes = [
 ];
 
 const CreateCampaign = () => {
+  const classes = useStyles();
   return (
-    <div style={{ padding: "15px", margin: "3em auto", maxWidth: "600px" }}>
-      <Typography variant="h4" align="center" gutterBottom color="secondary">
-        Create Your Campaign
-      </Typography>
-      <form>
-        <Paper style={{ padding: "15px" }}>
-          <Grid container alignItems="flex-start" spacing={2}>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                required
-                name="name"
-                type="text"
-                label="First Name"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                required
-                name="email"
-                type="text"
-                label="Email"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                required
-                name="description"
-                type="text area"
-                label="Project Description"
-                variant="outlined"
-                rows={4}
-                multiline
-              />
-            </Grid>
+    <>
+      <Grid item style={{ margin: "3em auto 0" }}>
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          style={{ height: "30em" }}
+        >
+          <div className={classes.mainImage} />
+        </Grid>
+      </Grid>
+      <div style={{ padding: "15px", margin: "3em auto", maxWidth: "600px" }}>
+        <Typography variant="h4" align="center" gutterBottom color="secondary">
+          Create Your Campaign
+        </Typography>
+        <form>
+          <Paper style={{ padding: "15px" }}>
+            <Grid container alignItems="flex-start" spacing={2}>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  required
+                  name="name"
+                  type="text"
+                  label="First Name"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  required
+                  name="email"
+                  type="text"
+                  label="Email"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  required
+                  name="description"
+                  type="text area"
+                  label="Project Description"
+                  variant="outlined"
+                  rows={4}
+                  multiline
+                />
+              </Grid>
 
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                required
-                name="projectType"
-                type="select"
-                select
-                label="Select"
-                helperText="Please select your Project Type"
-                variant="outlined"
-              >
-                {projectTypes.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  required
+                  name="projectType"
+                  type="select"
+                  select
+                  label="Select"
+                  helperText="Please select your Project Type"
+                  variant="outlined"
+                >
+                  {projectTypes.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  required
+                  name="funding"
+                  type="text"
+                  label="Funding Goal"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid item style={{ marginTop: 16 }}>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  type="submit"
+                  style={{ width: "150px" }}
+                >
+                  Submit
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item xs={6}>
-              <TextField
-                fullWidth
-                required
-                name="funding"
-                type="text"
-                label="Funding Goal"
-                variant="outlined"
-              />
-            </Grid>
-            <Grid item style={{ marginTop: 16 }}>
-              <Button
-                variant="contained"
-                color="secondary"
-                type="submit"
-                style={{width:"150px"}}
-              >
-                Submit
-              </Button>
-            </Grid>
-          </Grid>
-        </Paper>
-      </form>
-    </div>
+          </Paper>
+        </form>
+      </div>
+    </>
   );
 };
 
@@ -130,9 +156,3 @@ const mapStateToProps = (state) => {
   return {};
 };
 export default connect()(CreateCampaign);
-
-//first name
-//last name
-//project description
-//capaign goal
-//upload project image
